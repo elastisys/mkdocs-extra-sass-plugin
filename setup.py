9b4652e0e510c2568abf419ba9fc8e2fa79c285f
max-uho <maxturnheim@gmail.com>
2022-11-17 11:20:41 +0100

    added bs4 import to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mkdocs-extra-sass-plugin',
     install_requires=[
-        'libsass==0.22.0',
+        'libsass==0.22.0','beautifulsoup4==4.11.1'
     ],
     packages=find_packages(),
     entry_points={
